94ca02f4c6fbf20cede93dea0a5f59840874a31b
S Anand <root.node@gmail.com>
2010-12-02 09:15:09 +0000

    Using lxml instead of BeautifulSoup

--- ward-colorize.py
+++ ward-colorize.py
@@ -3,28 +3,20 @@
 #
 #---------------------------------------------------#
 import csv
-from BeautifulSoup import BeautifulSoup
-# Read in ward data
-ward_values = dict(csv.reader(open('ward.csv')))
+from lxml import etree
 
 # Load the SVG map
-svg = open('bbmp-ward.svg', 'r').read()
-# Load into Beautiful Soup
-soup = BeautifulSoup(svg, selfClosingTags=['defs','sodipodi:namedview'])
-# Find counties
-paths = soup.findAll('path')
+svg = etree.parse('bbmp-ward.svg')
+
+# Set the namespaces used
+ns = {'svg':'http://www.w3.org/2000/svg'}
 
 # Map colors
 colors = ["#F1EEF6", "#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77", "#980043"]
 
 # Color the counties based on unemployment rate
-for p in paths:
-	try:
-	     ward_value =  ward_values[p['id']]
-	     color = colors[int(ward_value)]
-	     p['fill'] = color
-	     #print p['id']		
-	except:
-	    continue
-# Output map
-print soup.prettify()
+for ward_id, ward_value in csv.reader(open('ward.csv')):
+    for element in svg.xpath('//svg:path[@id=%s]' % ward_id, namespaces=ns):
+        element.set('fill', colors[int(ward_value)])
+
+print etree.tostring(svg)
